ansible-community/collection_migration

commit 6db33bb66ff6695a440648a6b7ba0e959478964d

Add .cache/errors.log (#24)

migrate.py
@@ -6,6 +6,7 @@ import argparse
 import configparser
 import glob
 import itertools
+import logging
 import os
 import re
 import shutil
@@ -20,6 +21,7 @@ from importlib import import_module
 from string import Template
 
 from ansible.vars.reserved import is_reserved_name
+import logzero
 from logzero import logger
 
 from baron.parser import ParsingError
@@ -40,6 +42,9 @@ PLUGIN_EXCEPTION_PATHS = {'modules': 'lib/ansible/modules', 'module_utils': 'lib
 
 RAW_STR_TMPL = "r'''{str_val}'''"
 STR_TMPL = "'''{str_val}'''"
+
+
+logzero.logfile(os.path.join(VARDIR, 'errors.log'), loglevel=logging.ERROR)
 
 
 core = {}

commit 0c1b332d43cb4a45016aea52b5c8181c9b27af34

Ensure that log dir exists
Fixes a regression introduced by
6db33bb66ff6695a440648a6b7ba0e959478964d:
https://github.com/ansible/collection_migration/runs/221243282#step:8:23

migrate.py
@@ -44,6 +44,7 @@ RAW_STR_TMPL = "r'''{str_val}'''"
 STR_TMPL = "'''{str_val}'''"
 
 
+os.makedirs(VARDIR, exist_ok=True)
 logzero.logfile(os.path.join(VARDIR, 'errors.log'), loglevel=logging.ERROR)
 
 