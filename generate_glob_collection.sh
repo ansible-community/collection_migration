eefe9b24a2300c28296eb273c41784bf665e1266
Brian Coca <brian.coca+git@gmail.com>
2019-09-18 09:47:21 -0400

    QoL improvements

--- generate_glob_collection.sh
+++ generate_glob_collection.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
+# script takes 2 positional parameters:
+# $1 = (required) ansible repo to search for files
+# $2 = (optional) name of collection to use in output
+
 set -eu
+set -o pipefail
 
 if [ -d $1 ]; then
 
 	pullfrom=()
-	pullfrom+=$(find $1/lib/ansible/plugins/* -maxdepth 1 -type d |grep -v __)
+	pullfrom+=$(find $1/lib/ansible/plugins/* -maxdepth 1 -type d |grep -v __ | sort)
 	pullfrom+=("$1/lib/ansible/modules" "$1/lib/ansible/module_utils")
 
 	files=()
 	for t in ${pullfrom[@]}
 	do
 		files+=" "
-		files+=$(find $t -type f|grep -v '.pyc'|grep -v '.pyo')
+		files+=$(find $t -type f|grep -v '.pyc'|grep -v '.pyo'|sort)
 	done
 
 	category=''
-	echo 'glob:'
+	collection=${2:-glob}
+	echo "${collection}:"
 	for f in ${files}
 	do
 		f=${f#$1/lib/ansible/plugins/}
 		f=${f#$1/lib/ansible/}
 
-		cat=$( echo $f |cut -f1 -d'/')
+		cat=$(echo $f |cut -f1 -d'/')
 		token=${f#"$cat/"}
 
 		# only print on cat change, files should already be grouped by category
 		if [ "$cat" != "$category" ]; then
-			echo "  $cat:"
+			echo "  ${cat}:"
 			category=$cat
 		fi
 
