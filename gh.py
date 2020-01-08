2d4b4f01f012403e6cc3ca4b985e1f8282ef74f2
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-01-08 17:33:48 +0100

    Adjust GH client to consume new octomachinery API

--- gh.py
+++ gh.py
@@ -9,11 +9,21 @@ import os
 import pathlib
 from typing import Union
 
+from aiohttp.client import ClientSession
 import gidgethub
 from logzero import logger
 
 from octomachinery.github.api.app_client import GitHubApp
 from octomachinery.github.config.app import GitHubAppIntegrationConfig
+
+
+def provision_http_session(async_method):
+    """Inject aiohttp client session into method keyword args."""
+    async def async_method_wrapper(self, *args, **kwargs):
+        async with ClientSession() as http_session:
+            kwargs['http_session'] = http_session
+            return await async_method(self, *args, **kwargs)
+    return async_method_wrapper
 
 
 @dataclass(frozen=True)
@@ -38,7 +48,7 @@ class GitHubOrgClient:
             self.github_app_private_key_path,
         ).expanduser().resolve().read_text()
 
-    def _get_github_app(self):
+    def _get_github_app(self, http_session: ClientSession):
         """Initialize a GitHub App instance with creds."""
         github_app_config = GitHubAppIntegrationConfig(
             app_id=self._read_app_id(),
@@ -48,11 +58,11 @@ class GitHubOrgClient:
             app_version='1.0',
             app_url='https://github.com/ansible/collection_migration',
         )
-        return GitHubApp(github_app_config)
+        return GitHubApp(github_app_config, http_session)
 
-    async def _get_github_client(self):
+    async def _get_github_client(self, http_session: ClientSession):
         """Return a GitHub API client for the target org."""
-        github_app = self._get_github_app()
+        github_app = self._get_github_app(http_session)
         try:
             github_app_installations = await github_app.get_installations()
         except gidgethub.BadRequest:
@@ -66,11 +76,16 @@ class GitHubOrgClient:
             ),
             None,
         )
-        return target_github_app_installation.get_github_api_client()
+        return target_github_app_installation.api_client
 
-    async def create_repo_if_not_exists(self, repo_name):
+    @provision_http_session
+    async def create_repo_if_not_exists(
+            self, repo_name: str,
+            *,
+            http_session: ClientSession,
+    ):
         """Ensure that the repo exists under the org."""
-        github_api = await self._get_github_client()
+        github_api = await self._get_github_client(http_session)
         with contextlib.suppress(gidgethub.InvalidField):
             await github_api.post(
                 f'/orgs/{self.github_org_name}/repos',
@@ -83,10 +98,15 @@ class GitHubOrgClient:
                 f'/{repo_name}.git'
             )
 
-    async def get_org_repo_token(self, repo_name):
+    @provision_http_session
+    async def get_org_repo_token(
+            self, repo_name: str,
+            *,
+            http_session: ClientSession,
+    ) -> str:
         """Return an access token once the repo exists."""
         await self.create_repo_if_not_exists(repo_name)
-        return str((await self._get_github_client())._token)
+        return str((await self._get_github_client(http_session))._token)
 
     async def get_git_repo_token(self, repo_name):
         """Generate a Git repo URL with creds after ensuring repo existence."""
@@ -103,14 +123,19 @@ class GitHubOrgClient:
     def sync_provision_deploy_key_to(self, repo_name: str) -> int:
         return asyncio.run(self.provision_deploy_key_to(repo_name))
 
-    async def provision_deploy_key_to(self, repo_name: str) -> int:
+    @provision_http_session
+    async def provision_deploy_key_to(
+            self, repo_name: str,
+            *,
+            http_session: ClientSession,
+    ) -> int:
         """Add deploy key to the repo."""
         await self.create_repo_if_not_exists(repo_name)
 
         dpl_key = self.deployment_rsa_pub_key
         dpl_key_repr = dpl_key.split(' ')[1]
         dpl_key_repr = '...'.join((dpl_key_repr[:16], dpl_key_repr[-16:]))
-        github_api = await self._get_github_client()
+        github_api = await self._get_github_client(http_session)
         api_resp = await github_api.post(
             '/repos/{owner}/{repo}/keys',
             url_vars={
@@ -132,9 +157,14 @@ class GitHubOrgClient:
     def sync_drop_deploy_key_from(self, repo_name: str, key_id: int):
         return asyncio.run(self.drop_deploy_key_from(repo_name, key_id))
 
-    async def drop_deploy_key_from(self, repo_name: str, key_id: int):
+    @provision_http_session
+    async def drop_deploy_key_from(
+            self, repo_name: str, key_id: int,
+            *,
+            http_session: ClientSession,
+    ) -> None:
         """Add deploy key to the repo."""
-        github_api = await self._get_github_client()
+        github_api = await self._get_github_client(http_session)
         await github_api.delete(
             '/repos/{owner}/{repo}/keys/{key_id}',
             url_vars={
